60883a3e9fe4297f17c1f6604c63e69048259370
cecilelecerf <c.lecerf.cecile@gmail.com>
2026-02-06 11:26:26 +0100

    delete

--- web/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"acJ97kFZ28p1EDMx8tnTWParlr8V8CnnBd9NGRCyf4s=\"\n}"
--- web/.next/dev/static/chunks/src_app_page_tsx_c0de8dc1._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/5e669_next_dist_c1321e20._.js"
-],
-    source: "dynamic"
-});

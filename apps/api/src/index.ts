f86117e8e4ad19c9bd5a124554edf70921e75ddf
cecilelecerf <c.lecerf.cecile@gmail.com>
2026-03-14 16:19:31 +0100

    fix: nginx

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -14,7 +14,7 @@ app.use(
 );
 app.use(express.json());
 
-app.get("/test", async (req, res) => {
+app.get("/api/test", async (req, res) => {
   const users = await prisma.user.findMany({
     include: {
       clientProfile: true,

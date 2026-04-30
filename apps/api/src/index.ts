5e99154e96b6f0cc3ba5ba48de82fd669ef700b8
cecilelecerf <c.lecerf.cecile@gmail.com>
2026-04-30 16:49:56 +0200

    add metrics

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -5,7 +5,24 @@ import cors from "cors";
 import meetingRouter from "./routes/metting.route";
 import { errorHandler } from "./middlewares/error.middleware";
 import userRouter from "./routes/user.route";
+import { collectDefaultMetrics } from "prom-client";
+import express_prom_bundle from "express-prom-bundle";
 const app = express();
+
+collectDefaultMetrics();
+
+const metricsMiddleware = express_prom_bundle({
+  includeMethod: true,
+  includePath: true,
+  includeStatusCode: true,
+  normalizePath: [
+    ["/api/users/.*", "/api/users/#id"],
+    ["/api/meetings/.*", "/api/meetings/#id"],
+  ],
+});
+
+// 👆 métriques en premier
+app.use(metricsMiddleware);
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN || "http://localhost:5173",

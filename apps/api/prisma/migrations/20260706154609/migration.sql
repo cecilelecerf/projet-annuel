2623f25396853408819dbcc2f4f75434d2bfe415
cecilelecerf <c.lecerf.cecile@gmail.com>
2026-07-06 17:46:41 +0200

    migrations

---
+++ apps/api/prisma/migrations/20260706154609/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `clinicId` on the `director_clinic_profiles` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "director_clinic_profiles" DROP COLUMN "clinicId";

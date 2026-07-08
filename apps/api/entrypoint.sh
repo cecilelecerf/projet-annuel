3970a94d094faf387d6538483de6948213698f7a
cecilelecerf <c.lecerf.cecile@gmail.com>
2026-07-08 12:21:29 +0200

    add resend and read secret in entrypoint

--- apps/api/entrypoint.sh
+++ apps/api/entrypoint.sh
@@ -10,6 +10,22 @@ if [ -f /run/secrets/db_user ]; then
   export JWT_REFRESH_SECRET=$(cat /run/secrets/jwt_refresh_secret)
 fi
 
+if [ -f /run/secrets/aws_access_key_id ]; then
+  export AWS_ACCESS_KEY_ID=$(cat /run/secrets/aws_access_key_id)
+  export AWS_SECRET_ACCESS_KEY=$(cat /run/secrets/aws_secret_access_key)
+  export S3_BUCKET=$(cat /run/secrets/s3_bucket)
+  export S3_ENDPOINT=$(cat /run/secrets/s3_endpoint)
+  export ASSETS_BASE_URL=$(cat /run/secrets/assets_base_url)
+fi
+
+if [ -f /run/secrets/stripe_secret_key ]; then
+  export STRIPE_SECRET_KEY=$(cat /run/secrets/stripe_secret_key)
+  export STRIPE_WEBHOOK_SECRET=$(cat /run/secrets/stripe_webhook_secret)
+fi
+
+if [ -f /run/secrets/resend_api_key ]; then
+  export RESEND_API_KEY=$(cat /run/secrets/resend_api_key)
+fi
 
 echo "Checking DATABASE_URL..."
 if [ -z "$DATABASE_URL" ]; then
